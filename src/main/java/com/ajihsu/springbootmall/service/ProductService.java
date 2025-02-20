9017bd428b488a2ee86602b49bd0932075f6285b
AJI-Hsu <mrhsu950325@gmail.com>
2025-02-20 22:07:45 +0800

    product functions - getProducts(get product list)

--- src/main/java/com/ajihsu/springbootmall/service/ProductService.java
+++ src/main/java/com/ajihsu/springbootmall/service/ProductService.java
@@ -3,7 +3,10 @@ package com.ajihsu.springbootmall.service;
 import com.ajihsu.springbootmall.dto.ProductRequest;
 import com.ajihsu.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+    List<Product> getProducts();
     Product getProductById(Integer productId);
     Integer createProduct(ProductRequest productRequest);
     void updateProduct(Integer productId, ProductRequest productRequest);
